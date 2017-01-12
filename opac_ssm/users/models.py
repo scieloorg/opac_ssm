2642bfeeac24d1daf83f2201f71a4f2c64ea61fe
Juan Funez <juan.funez@gmail.com>
2017-01-12 11:40:45 -0200

    add travis.yml e docker-compose.yml para produção buil e dev ajustados
    FIX: #25
    FIX: #27
    FIX: #24

--- opac_ssm/users/models.py
+++ opac_ssm/users/models.py
@@ -7,20 +7,14 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
-from django.db.models import signals
-from tastypie.models import create_api_key
-
 
 @python_2_unicode_compatible
 class User(AbstractUser):
 
-    name = models.CharField(_('Name of User'), blank=True, max_length=255)
+    name = models.CharField(_('Name'), blank=True, max_length=255)
 
     def __str__(self):
         return self.username
 
     def get_absolute_url(self):
         return reverse('users:detail', kwargs={'username': self.username})
-
-
-signals.post_save.connect(create_api_key, sender=User)
