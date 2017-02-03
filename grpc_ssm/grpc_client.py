0343dc8f7cc819a0600d8b2c12d2a412ed1dc072
Jamil Atta Junior <atta.jamil@gmail.com>
2017-02-03 14:43:23 -0200

    Melhorias no exemplo do GRPC (#59)
    * Melhorias no exemplo do GRPC
    
    * Melhorias no exemplo do GRPC e mantendo campo bucket coerente com o modelo

--- grpc_ssm/grpc_client.py
+++ grpc_ssm/grpc_client.py
@@ -6,28 +6,45 @@ import grpc
 
 import opac_pb2
 
+SLEEP_TIME = 10
 
 def run():
     channel = grpc.insecure_channel('localhost:5000')
 
     stub = opac_pb2.AssetServiceStub(channel)
 
+    print("Obtendo o arquivo para ser enviado pelo GRPC")
+
     file = open('sample.txt', 'rb')
 
     filename = os.path.basename(getattr(file, 'name', None))
 
-    meta = "{'foo': 'bar', 'pickles': 'blaus'}" # String
+    meta = '{"foo": "bar", "pickles": "blaus"}' # String
+
+    print("Envida os metadados %s para como param metadata." % meta)
 
     task = stub.add_asset(opac_pb2.Asset(file=file.read(), filename=filename,
                                             type="txt", metadata=meta))
 
+    print("ID da task: %s" % task.id)
+
     task_state = stub.get_task_state(opac_pb2.TaskId(id=task.id))
 
-    time.sleep(5) # sleep 5 seconds
+    print("Verificando o estado da task: %s" % task_state)
+
+    print("Dormindo por %s segundos..." % SLEEP_TIME)
+
+    time.sleep(SLEEP_TIME) # sleep 10 seconds
+
+    print("Agora vou verificando o estado da task: %s" % task.id)
 
     task_info = stub.get_asset_info(opac_pb2.TaskId(id=task.id))
 
+    print("Informações da url do asset: %s" % task_info)
+
     asset = stub.get_asset(opac_pb2.TaskId(id=task.id))
+
+    print("Retornando os dados do Asset: ")
 
     print((task.id, task_state.state, task_info.url, task_info.url_path, asset))
 
