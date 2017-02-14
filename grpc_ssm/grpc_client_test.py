a396732d4e7f2146c2d477ed302ea9eefc29c2ea
Jamil Atta Junior <atta.jamil@gmail.com>
2017-02-14 16:33:03 -0200

    Renomeado o client para teste

---
+++ grpc_ssm/grpc_client_test.py
@@ -0,0 +1,56 @@
+#coding: utf-8
+#!/usr/bin/env python
+
+import os
+import time
+import grpc
+
+import opac_pb2
+
+SLEEP_TIME = 10
+
+def run():
+    channel = grpc.insecure_channel('localhost:5000')
+
+    stub = opac_pb2.AssetServiceStub(channel)
+
+    print("Obtendo o arquivo para ser enviado pelo GRPC")
+
+    file = open('sample.txt', 'rb')
+
+    filename = os.path.basename(getattr(file, 'name', None))
+
+    meta = '{"foo": "bar", "pickles": "blaus"}' # String
+
+    print("Envida os metadados %s como param metadata." % meta)
+
+    task = stub.add_asset(opac_pb2.Asset(file=file.read(), filename=filename,
+                                         type="txt", metadata=meta,
+                                         bucket="Bucket Sample"))
+
+    print("ID da task: %s" % task.id)
+
+    task_state = stub.get_task_state(opac_pb2.TaskId(id=task.id))
+
+    print("Verificando o estado da task: %s" % task_state)
+
+    print("Dormindo por %s segundos..." % SLEEP_TIME)
+
+    time.sleep(SLEEP_TIME) # sleep 10 seconds
+
+    task_state = stub.get_task_state(opac_pb2.TaskId(id=task.id))
+
+    print("Agora vou verificando o estado da task: %s, %s" % (task.id, task_state))
+
+    task_info = stub.get_asset_info(opac_pb2.TaskId(id=task.id))
+
+    print("Informações da url do asset: %s" % task_info)
+
+    asset = stub.get_asset(opac_pb2.TaskId(id=task.id))
+
+    print("Retornando os dados do Asset: ")
+
+    print((task.id, task_state.state, task_info.url, task_info.url_path, asset))
+
+if __name__ == '__main__':
+    run()
