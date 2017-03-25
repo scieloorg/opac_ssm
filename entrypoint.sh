c793f225f1e30c0123ffe92746529098d40696e6
Jamil Atta Junior <atta.jamil@gmail.com>
2017-03-24 21:20:28 -0300

    Adicionando o pgbouncer para criar um pool de conexão

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,27 +13,26 @@ if [ -z "$POSTGRES_USER" ]; then
     export POSTGRES_USER=postgres
 fi
 
-export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_USER
+export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@pgbouncer:6432/$POSTGRES_USER
 
 export CELERY_BROKER_URL=$REDIS_URL/0
 
-
-function postgres_ready(){
+function pgbouncer_ready(){
 python << END
 import sys
 import psycopg2
 try:
-    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
+    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="pgbouncer", port="6432")
 except psycopg2.OperationalError:
     sys.exit(-1)
 sys.exit(0)
 END
 }
 
-until postgres_ready; do
-  >&2 echo "Postgres is unavailable - sleeping"
+until pgbouncer_ready; do
+  >&2 echo "PGBouncer is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "Postgres is up - continuing..."
+>&2 echo "PGBouncer is up - continuing..."
 exec $cmd
